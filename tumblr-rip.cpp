b69100ebf6a565eba31c16e88d46ba69baf73773
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-09-09 12:23:25 +0000

    image downloading download images embedded in captions

--- tumblr-rip.cpp
+++ tumblr-rip.cpp
@@ -6,14 +6,56 @@
 #include <thread>
 #include <chrono>
 #include <regex>
+#include <queue>
 
 constexpr char HOST[15] = "api.tumblr.com";
 constexpr char BASE_PATH[10] = "/v2/blog/";
 constexpr size_t LIMIT = 50;
 namespace http = httplib;
+enum protocol { HTTPS, HTTP };
 
 inline std::string construct_PATH(const char* api_key, const char* blog, size_t limit = 20, size_t offset = 0){
     return std::string(BASE_PATH)+blog+"/posts?api_key="+api_key+"&limit="+std::to_string(limit)+"&offset="+std::to_string(offset);
+}
+
+inline const std::smatch get_URL_parts(std::string &url){
+    const auto pat = std::regex(R"s((?:https?|ftp)://([^/\r\n]+)(/[^\r\n]*)?)s",
+        std::regex_constants::icase);
+    std::smatch m;
+    std::regex_search(url, m, pat);
+    return m;
+}
+
+void batch_download(protocol p, std::string host, std::vector<http::Request> requests){
+    //http::Client *cli;
+    std::unique_ptr<http::Client> cli(
+        p == HTTPS 
+        ? new http::SSLClient(host.c_str())
+        : new http::Client(host.c_str())
+    );
+    cli->set_keep_alive_max_count(8);
+    cli->follow_location(true);
+
+    std::vector<http::Response> responses;
+    if(cli->send(requests, responses)){
+        const size_t len = requests.size();
+        try{
+            for (size_t i = 0; i < len; i++){
+                const http::Response &res = responses.at(i);
+                const http::Request &req = requests.at(i);
+                if(res.status != 200)
+                    continue;
+                std::string filename = "data/assets"
+                    + req.path.substr(req.path.rfind('/'));
+                std::ofstream asset(filename.c_str(), std::ofstream::binary);
+                asset.write(res.body.c_str(), res.body.size());
+                asset.close();
+                std::cerr << "saved " << filename << std::endl;
+            }
+        } catch(std::out_of_range){
+            return;
+        }
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -102,13 +144,24 @@ int main(int argc, char const *argv[])
                 else
                     continue;
 
+                std::map<std::string, std::vector<http::Request>> hosts_downloads;
+
                 for (auto it = std::sregex_iterator(content.begin(), content.end(), url_pat);
                     it != std::sregex_iterator();
                     ++it)
                 {
                     std::smatch match = *it;
-                    if(http::detail::find_content_type(match.str()))
-                        std::cerr << match.str() << std::endl;
+                    std::string match_s = match.str();
+                    if(http::detail::find_content_type(match_s)){
+                        //std::cerr << match_s << std::endl;
+                        std::smatch URL_parts = get_URL_parts(match_s);
+                        http::Get(hosts_downloads[URL_parts[1].str()], URL_parts[2].str().c_str(), headers);
+                    }
+                }
+                for(auto& download_queue : hosts_downloads){
+                    const std::string &host = download_queue.first;
+                    std::vector<http::Request> &queue = download_queue.second;
+                    std::thread(batch_download, HTTPS, host, queue).detach();
                 }
             }
             offset += LIMIT;
